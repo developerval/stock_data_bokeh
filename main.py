66f9bf588ec79582af9bde695f57fb0c26d36460
developerval <valentine.bott@gmail.com>
2020-06-08 21:59:46 +0100

    changing couple of bits on how the graph is built

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pandas_datareader import data
 import datetime
 from bokeh.plotting import figure, show, output_file
+from bokeh.embed import components
+from bokeh.resources import CDN
 
 
 def inc_dec(c, o):
@@ -14,7 +16,7 @@ def inc_dec(c, o):
 
 
 start = datetime.datetime(2019, 1, 1)
-end = datetime.datetime(2019, 12, 31)
+end = datetime.datetime(2019, 3, 31)
 hours_12 = 12*60*60*1000
 
 df = data.DataReader('TSLA', 'yahoo', start, end)
@@ -24,17 +26,23 @@ df['Middle'] = (df.Open + df.Close) / 2
 df['Height'] = abs(df.Close - df.Open)
 
 p = figure(x_axis_type='datetime', width=1000,
-           height=300, sizing_mode='scale_width')
-p.title.text = "Tesla Stock Information for 2019"
+           height=300, sizing_mode='scale_width', title='Tesla Stock Price 2019')
 p.grid.grid_line_alpha = 0.25
 
 p.segment(df.index, df.High, df.index, df.Low, color='black')
 
 p.rect(df.index[df.Status == 'Increase'], df.Middle[df.Status == 'Increase'], hours_12,
-       df.Height[df.Status == 'Increase'], fill_color='green', line_color='black')
+       df.Height[df.Status == 'Increase'], fill_color='#3CB371', line_color='black')
 
 p.rect(df.index[df.Status == 'Decrease'], df.Middle[df.Status == 'Decrease'], hours_12,
-       df.Height[df.Status == 'Decrease'], fill_color='red', line_color='black')
+       df.Height[df.Status == 'Decrease'], fill_color='#FF6347', line_color='black')
 
-output_file('CS.html')
+output_file("CS.html")
 show(p)
+
+bokeh_tsla, div_tsla = components(p)
+
+# print(div_tsla)
+
+cdn_js = CDN.js_files[0]
+# # cdn_css = CDN.css_files
